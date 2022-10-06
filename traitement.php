035ad5d7c677ce717eec59504085a82e98af215e
mayados <mariannedos.cours@gmail.com>
2022-10-06 14:47:27 +0200

    Correction d'un bug

--- traitement.php
+++ traitement.php
@@ -5,7 +5,7 @@
     /* Vérifier si la clé "submit" correspond bien à l'attribut "name" du bouton du formulaire : cela limite l'accès à traitement.php. Seules les requêtes HTTP provenant de la soumission de notre formulairesont acceptées */
     if(isset($_POST['submit'])){
         /* Vérification de l'intégralité des valeurs transmises dans le tableau $_POST en fonction de celles que nous attendons */
-        $_name = filter_input(INPUT_POST, "name", FILTER_SANITIZE_STRING);
+        $name = filter_input(INPUT_POST, "name", FILTER_SANITIZE_STRING);
         $price = filter_input(INPUT_POST, "price", FILTER_VALIDATE_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
         $qtt = filter_input(INPUT_POST, "qtt", FILTER_VALIDATE_INT);
 
