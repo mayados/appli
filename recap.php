90a213177ecf7dd479f4020445905d8baa55082a
mayados <mariannedos.cours@gmail.com>
2022-10-11 10:01:36 +0200

    commentaire avec possibilité de refresh en html

--- recap.php
+++ recap.php
@@ -9,6 +9,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- <meta http-equiv="refresh" content="3"> -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
     <link rel="stylesheet" href="css/recapitulatif.css">
@@ -43,6 +44,7 @@
                         foreach($_SESSION['products'] as $index => $product){
                             $ref = $index;
                             $quantiteProduit = $product['qtt'];
+                            $produit = $product['name'];
                             echo "<tr>",
                                     "<td>".$index."</td>",
                                     "<td>".$product['name']."</td>",
@@ -52,7 +54,7 @@
                                     <a href='traitement.php?action=baisserQuantite&ref=$index'>-</a>".$quantiteProduit."<a href='traitement.php?action=augmenterQuantite&ref=$index'>+</a> </td>",
                                     "<td>".number_format($product['total'], 2, ",", "&nbsp;")."&nbsp;€</td>",
                                     /* La référence du lien guide vers la page retrait_produit.php. On indique que le retrait correspond à l'index auquel nous sommes (du tableau products) */
-                                    "<td><a href='traitement.php?action=suppprimerProduit&ref=$index'>Supprimer</a></td>",
+                                    "<td><a href='traitement.php?action=suppprimerProduit&ref=$index&produit=$produit'>Supprimer</a></td>",
                                 "</tr>";
 
                             $totalGeneral += $product['total'];
